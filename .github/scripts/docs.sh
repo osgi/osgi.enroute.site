6f04d10655898cdad8843b52b52ece7d9841c7cf
BJ Hargrave <hargrave@us.ibm.com>
2021-05-12 10:28:09 -0400

    build: Update to ruby/setup-ruby action
    Signed-off-by: BJ Hargrave <hargrave@us.ibm.com>

--- .github/scripts/docs.sh
+++ .github/scripts/docs.sh
@@ -1,11 +1,6 @@
 #!/usr/bin/env bash
 ruby --version
 gem --version
-gem install bundler -v '~> 2.0'
 bundle --version
 
-export BUNDLE_GEMFILE=$PWD/Gemfile
-bundle config --local deployment 'true'
-bundle config --local path 'vendor/bundle'
-bundle install --jobs=3 --retry=3
 bundle exec jekyll build
